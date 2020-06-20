0e011570bf886b5c39c02fa22606456804e62617
Lifei Li <lupuswere@gmail.com>
2020-06-20 14:03:46 -0700

    Remove redis config

--- src/main/java/net/lilifei/app/password/spring/RedisConfig.java
+++
@@ -1,8 +0,0 @@
-package net.lilifei.app.password.spring;
-
-import org.springframework.cloud.aws.cache.config.annotation.CacheClusterConfig;
-import org.springframework.cloud.aws.cache.config.annotation.EnableElastiCache;
-
-@EnableElastiCache({@CacheClusterConfig(name = "spring-session")})
-public class RedisConfig {
-}
